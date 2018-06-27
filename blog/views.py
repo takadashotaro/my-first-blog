886d65288394964555611618ebb38d5327c24895
takadashotaro2 <gathering.of.my.sell@gmail.com>
2018-06-27 12:33:13 +0900

    change for blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
